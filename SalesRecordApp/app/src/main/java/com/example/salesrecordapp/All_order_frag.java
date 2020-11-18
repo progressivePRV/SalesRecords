1b94f2031207fd7ac852811eebc0af8063a07064
Monesa <mthogulu@uncc.edu>
2020-11-18 01:01:02 -0500

    Filter and arraylist to 100 records, swipe up and load more functionality is implemented and sorting is implemented

--- SalesRecordApp/app/src/main/java/com/example/salesrecordapp/All_order_frag.java
+++ SalesRecordApp/app/src/main/java/com/example/salesrecordapp/All_order_frag.java
@@ -23,6 +23,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -60,7 +61,10 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
     private SwipeRefreshLayout swipeRefreshLayout;
     Gson gson = new Gson();
     User user;
+    String globalQuery = "";
+    int page = 1;
     ArrayList<Order> orderArrayList = new ArrayList<>();
+    ArrayList<Order> memoryArrayList = new ArrayList<>();
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -71,6 +75,8 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
 
     private EditText numberEditTextCon, FilterOrderName;
     private Spinner conditionSpinner;
+
+    private RadioButton radioButton, radioButton2;
 
     public All_order_frag() {
         // Required empty public constructor
@@ -109,21 +115,8 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
         View view = inflater.inflate(R.layout.fragment_all_order_frag, container, false);
         // Inflate the layout for this fragment
 
-
         viewModel = new ViewModelProvider(this).get(AppViewModel.class);
 
-//                Order o = new Order();
-//                o.user_id="user1";
-//                o._id="2";
-//                o.item_type="type1";
-//                o.order_date="11/11/2020";
-//                o.unit_cost= (float) 10.0;
-//                o.units_sold=2;
-//                o.total=20.0;
-//                  viewModel.InsertOrder(o);   ////////// pass order to insert
-
-
-        //********************************** FindSpecificOrder testing
         preferences = getActivity().getSharedPreferences("TokeyKey", 0);
 
         String token_key = preferences.getString("TOKEN_KEY", null);
@@ -131,12 +124,6 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
         String pro =  preferences.getString("USER",null);
         user = gson.fromJson(pro, User.class);
 
-
-
-        // this is for finsing a specific order.
-//        new FindSpecificOrder().execute("1",);
-
-        //As this is creating a new view, so getting all the orders from the server.
         return view;
     }
 
@@ -150,16 +137,8 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
         recyclerView.setLayoutManager(layoutManager);
 
         // specify an adapter (see also next example)
-        mAdapter = new OrderAdapter(orderArrayList, All_order_frag.this);
+        mAdapter = new OrderAdapter(memoryArrayList, All_order_frag.this);
         recyclerView.setAdapter(mAdapter);
-
-        numberEditTextCon = getView().findViewById(R.id.numberEditTextCon);
-        FilterOrderName = getView().findViewById(R.id.FilterOrderName);
-        conditionSpinner = getView().findViewById(R.id.conditionSpinner);
-
-        numberEditTextCon.setVisibility(EditText.INVISIBLE);
-        FilterOrderName.setVisibility(EditText.INVISIBLE);
-        conditionSpinner.setVisibility(Spinner.INVISIBLE);
 
         Spinner spinner = (Spinner) getView().findViewById(R.id.filterSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
@@ -179,18 +158,49 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sortSpinner.setAdapter(adapter2);
 
+        numberEditTextCon = getView().findViewById(R.id.numberEditTextCon);
+        FilterOrderName = getView().findViewById(R.id.FilterOrderName);
+        conditionSpinner = getView().findViewById(R.id.conditionSpinner);
+
+        numberEditTextCon.setVisibility(EditText.INVISIBLE);
+        FilterOrderName.setVisibility(EditText.INVISIBLE);
+        conditionSpinner.setVisibility(Spinner.INVISIBLE);
+
+        radioButton = getView().findViewById(R.id.radioButton);
+        radioButton2 = getView().findViewById(R.id.radioButton2);
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position == 1){
+                resetEveryField();
+                if(position == 0){
+                    FilterOrderName.setVisibility(EditText.INVISIBLE);
+                    conditionSpinner.setVisibility(Spinner.INVISIBLE);
+                    numberEditTextCon.setVisibility(EditText.INVISIBLE);
+                } else if(position == 1 || position == 2){
                     Log.d("demo",position+" is entering inside");
                     FilterOrderName.setVisibility(EditText.VISIBLE);
                     conditionSpinner.setVisibility(Spinner.INVISIBLE);
                     numberEditTextCon.setVisibility(EditText.INVISIBLE);
-                }else if(position == 3){
+                }else{
                     FilterOrderName.setVisibility(EditText.INVISIBLE);
                     conditionSpinner.setVisibility(Spinner.VISIBLE);
                     numberEditTextCon.setVisibility(EditText.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        sortSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    radioButton.setChecked(false);
+                    radioButton2.setChecked(false);
                 }
             }
 
@@ -203,11 +213,229 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
             @Override
             public void onRefresh() {
                 Log.d("demo","recyclerview refreshing");
+                if(page > 2){
+                    page = page - 1;
+                    for(int i=memoryArrayList.size()-1; i<0; i++){
+                        memoryArrayList.remove(i);
+                        if(memoryArrayList.size() == 50){
+                            break;
+                        }
+                    }
+                    if(globalQuery.equals("")){
+                        new GetOrdersFromServer(true).execute("");
+                    }else{
+                        new GetOrdersFromServer(true).execute(globalQuery);
+                    }
+                }else{
+                    Toast.makeText(getActivity(), "No more records to be loaded. You are already on the top!", Toast.LENGTH_SHORT).show();
+                }
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
 
-        new GetOrdersFromServer().execute();
+        new GetOrdersFromServer(false).execute("");
+
+        //Search filter button functionalities
+        getView().findViewById(R.id.searchFilter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //This is for filter;
+                String query = "";
+//                filter=order_date&filterValue=24&sortBy=order_date&sortOrder=desc&page=1
+                int position = spinner.getSelectedItemPosition();
+                int condition = 0;
+                switch (position){
+                    case 0:
+                        query = "";
+                        break;
+                    case 1:
+                        query = "";
+                        if(FilterOrderName.getText().toString().equals("")){
+                            Toast.makeText(getActivity(), "Search Text cannot be empty", Toast.LENGTH_SHORT).show();
+                            FilterOrderName.setError("Cannot be empty");
+                        }else{
+                            query = "filter=item_type&filterValue="+FilterOrderName.getText().toString();
+                        }
+                        break;
+                    case 2:
+                        query = "";
+                        if(FilterOrderName.getText().toString().equals("")){
+                            Toast.makeText(getActivity(), "Search Text cannot be empty", Toast.LENGTH_SHORT).show();
+                            FilterOrderName.setError("Cannot be empty");
+                        }else{
+                            query = "filter=order_date&filterValue="+FilterOrderName.getText().toString();
+                        }
+                        break;
+                    case 3:
+                        query = "";
+                        condition = condSpinner.getSelectedItemPosition();
+                        if(numberEditTextCon.getText().toString().equals("")){
+                            Toast.makeText(getActivity(), "Search Text cannot be empty", Toast.LENGTH_SHORT).show();
+                            numberEditTextCon.setError("Cannot be empty");
+                        }else{
+                            String filterComparator = getFilterComparator(condition);
+                            query = "filter=units_sold&filterValue="
+                                    +numberEditTextCon.getText().toString()
+                                    +"&filterComparator="+filterComparator;
+                        }
+                        break;
+                    case 4:
+                        query = "";
+                        condition = condSpinner.getSelectedItemPosition();
+                        if(numberEditTextCon.getText().toString().equals("")){
+                            Toast.makeText(getActivity(), "Search Text cannot be empty", Toast.LENGTH_SHORT).show();
+                            numberEditTextCon.setError("Cannot be empty");
+                        }else{
+                            String filterComparator = getFilterComparator(condition);
+                            query = "filter=unit_cost&filterValue="
+                                    +numberEditTextCon.getText().toString()
+                                    +"&filterComparator="+filterComparator;
+                        }
+                        break;
+                    case 5:
+                        query = "";
+                        condition = condSpinner.getSelectedItemPosition();
+                        if(numberEditTextCon.getText().toString().equals("")){
+                            Toast.makeText(getActivity(), "Search Text cannot be empty", Toast.LENGTH_SHORT).show();
+                            numberEditTextCon.setError("Cannot be empty");
+                        }else{
+                            String filterComparator = "";
+
+                            query += "filter=total&filterValue="
+                                    +numberEditTextCon.getText().toString()
+                                    +"&filterComparator="+filterComparator;
+                        }
+                        break;
+                }
+
+                //This is for sorting
+
+                int sortPosition = sortSpinner.getSelectedItemPosition();
+//                sortBy=order_date&sortOrder=desc
+                String sortQuery = "";
+
+                switch (sortPosition){
+                    case 0:
+                        sortQuery = "";
+                        break;
+                    case 1:
+                        sortQuery = "";
+                        if(radioButton.isChecked()){
+                            sortQuery = "sortBy=item_type&sortOrder=desc";
+                        }else{
+                            sortQuery = "sortBy=item_type&sortOrder=asc";
+                        }
+                        break;
+                    case 2:
+                        sortQuery = "";
+                        if(radioButton.isChecked()){
+                            sortQuery = "sortBy=order_date&sortOrder=desc";
+                        }else{
+                            sortQuery = "sortBy=order_date&sortOrder=asc";
+                        }
+                        break;
+                    case 3:
+                        sortQuery = "";
+                        if(radioButton.isChecked()){
+                            sortQuery = "sortBy=units_sold&sortOrder=desc";
+                        }else{
+                            sortQuery = "sortBy=units_sold&sortOrder=asc";
+                        }
+                        break;
+                    case 4:
+                        sortQuery = "";
+                        if(radioButton.isChecked()){
+                            sortQuery = "sortBy=unit_cost&sortOrder=desc";
+                        }else{
+                            sortQuery = "sortBy=unit_cost&sortOrder=asc";
+                        }
+                        break;
+                    case 5:
+                        sortQuery = "";
+                        if(radioButton.isChecked()){
+                            sortQuery = "sortBy=total&sortOrder=desc";
+                        }else{
+                            sortQuery = "sortBy=total&sortOrder=asc";
+                        }
+                        break;
+                }
+                page = 1;
+                memoryArrayList.clear();
+
+                if(query.equals("")&&sortQuery.equals("")){
+                    globalQuery = "";
+                    new GetOrdersFromServer(false).execute("");
+                }else if(query.equals("") && !sortQuery.equals("")){
+                    globalQuery = "&"+sortQuery;
+                    new GetOrdersFromServer(false).execute(globalQuery);
+                }else if(!query.equals("") && sortQuery.equals("")){
+                    globalQuery = "&"+query;
+                    new GetOrdersFromServer(false).execute(globalQuery);
+                }else if(!query.equals("") && !sortQuery.equals("")){
+                    globalQuery = "&"+query+"&"+sortQuery;
+                    new GetOrdersFromServer(false).execute(globalQuery);
+                }
+
+            }
+        });
+
+        //resetFilter button functionalities
+        getView().findViewById(R.id.resetFilter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.setSelection(0);
+                resetEveryField();
+                sortSpinner.setSelection(0);
+                radioButton.setChecked(false);
+                radioButton2.setChecked(false);
+                memoryArrayList.clear();
+                page = 1;
+                new GetOrdersFromServer(false).execute("");
+            }
+        });
+
+        getView().findViewById(R.id.buttonLoadMore).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(memoryArrayList.size() == 100){
+                    for(int i=0; i<memoryArrayList.size(); i++){
+                        memoryArrayList.remove(i);
+                        if(i==49){
+                            break;
+                        }
+                    }
+                }
+                page = page+1;
+                if(globalQuery.equals("")){
+                    new GetOrdersFromServer(false).execute("");
+                }else{
+                    new GetOrdersFromServer(false).execute(globalQuery);
+                }
+            }
+        });
+    }
+
+    public String getFilterComparator(int condition){
+        String filterComparator = "";
+        if(condition == 0){
+            filterComparator = "eq";
+        }else if(condition == 1){
+            filterComparator = "gte";
+        }else if(condition == 2){
+            filterComparator = "gt";
+        }else if(condition == 3){
+            filterComparator = "lte";
+        }else if(condition == 4){
+            filterComparator = "lt";
+        }
+        return filterComparator;
+    }
+
+    public void resetEveryField(){
+        FilterOrderName.setText("");
+        numberEditTextCon.setText("");
+        conditionSpinner.setSelection(0);
     }
 
     @Override
@@ -230,13 +458,18 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
 
     class GetOrdersFromServer extends AsyncTask<String ,Void,String> {
         String result = "", error = "";
-
+        boolean isTop;
+        GetOrdersFromServer(boolean isTop){
+            this.isTop = isTop;
+        }
         @Override
         protected String doInBackground(String... strs) {
+            orderArrayList.clear();
             final OkHttpClient client = new OkHttpClient();
+            Log.d("demo" , getResources().getString(R.string.endPointUrl)+"sales?page="+page+strs[0]);
 
             Request request = new Request.Builder()
-                    .url(getResources().getString(R.string.endPointUrl)+"sales?page=1")
+                    .url(getResources().getString(R.string.endPointUrl)+"sales?page="+page+strs[0])
                     .header("Authorization", "Bearer "+ preferences.getString("TOKEN_KEY", null))
                     .build();
             String responseValue = null;
@@ -280,7 +513,22 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
                         Log.d(TAG, "onPostExecute: error while parsing the jason array of orders");
                     }
                     if(orderArrayList.size() > 0){
+                        if(isTop == true){
+                            List<Order> temp = new ArrayList<>(memoryArrayList);
+                            memoryArrayList.clear();
+                            Log.d("demo","temp size is "+temp.size());
+                            memoryArrayList.addAll(orderArrayList);
+                            memoryArrayList.addAll(temp);
+                        }else{
+                            memoryArrayList.addAll(orderArrayList);
+                        }
+                        Log.d("demo","memoryArrayList size is "+memoryArrayList.size());
                         mAdapter.notifyDataSetChanged();
+                        if(isTop == false){
+                            recyclerView.scrollToPosition(51);
+                        }else{
+                            recyclerView.scrollToPosition(0);
+                        }
                     }else{
                         mAdapter.notifyDataSetChanged();
                         Toast.makeText(getActivity(), "Sorry no orders available!", Toast.LENGTH_SHORT).show();
@@ -292,6 +540,8 @@ public class All_order_frag extends Fragment implements OrderAdapter.InteractWit
                         JSONObject root = new JSONObject(error);
                         int errorCode = root.getInt("errorCode");
                         switch (errorCode) {
+                            case 102:
+                                Toast.makeText(getActivity(), "No More Records Found", Toast.LENGTH_SHORT).show();
                             case 103:
                                 Toast.makeText(getActivity(), "Token was not provided for request", Toast.LENGTH_SHORT).show();
                                 Log.d(TAG, "onPostExecute: token was not provided for request");
